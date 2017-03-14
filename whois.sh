ccfa87deddb79fea3235640b94dd5d05f0454cda
benzbrake <github-benzBrake@woai.ru>
2017-03-14 10:21:46 +0800

    Add Specify Host & Port Functions

--- whois.sh
+++ whois.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
-ARGUMENT="$1"
-test -z "$WHOIS_WORKING_DIR" && WHOIS_WORKING_DIR=$(dirname "$0")
+while echo $1 | grep -q ^-; do
+	eval $( echo $1 | sed 's/^-//' )=$2
+	shift
+	shift
+done
+
+! test -z "$host" && HOST=$host
+! test -z "$H" && HOST=$H
+! test -z "$port" && PORT=$port
+! test -z "$P" && PORT=$P
+! test -z "$iana" && IANA=$iana
+! test -z "$I" && IANA=$I
+! test -z "$IANA" && { HOST="whois.iana.org"; PORT=43; }
+ARGUMENT=$@
 test -z "$ARGUMENT" && { echo "Arguments Error."; exit 1;}
+test -z "$WHOIS_WORKING_DIR" && WHOIS_WORKING_DIR=$(dirname "$0")
 function prep ()
 {
 	echo "$1" | sed -e 's/^ *//g' -e 's/ *$//g' | sed -n '1 p'
@@ -12,16 +25,23 @@ DOMAIN=$(echo "$ARGUMENT" | sed 's#.*http.*//##;s#/.*##')
 	if [[ "$IP" == "$DOMAIN" ]]; then
 		whois "$DOMAIN"
 	else
-		TLD=$(echo $DOMAIN | sed 's#.*\.##')
-		if [ -e "${WHOIS_WORKING_DIR}/api/${TLD}.sh" ]; then
-			# WEB WHOIS
-			RESULT=$("${WHOIS_WORKING_DIR}/api/${TLD}.sh" "$DOMAIN")
+		if [ "$HOST" != "" ] && [ "$PORT" != "" ]; then
+			whois -h $HOST -p $PORT $DOMAIN
 		else
-			# NON WEB WHOIS
-			RESULT=$(whois "$DOMAIN")
-			echo "$RESULT" | grep -i "no whois server" > /dev/null
-			if [ $? -eq 0 ]; then
-				RESULT=$("${WHOIS_WORKING_DIR}/api/getwhois.sh" "$DOMAIN")
+			TLD=$(echo $DOMAIN | sed 's#.*\.##')
+			if [ -e "${WHOIS_WORKING_DIR}/api/${TLD}.sh" ]; then
+				# WEB WHOIS
+				RESULT=$("${WHOIS_WORKING_DIR}/api/${TLD}.sh" "$DOMAIN")
+			else
+				# NON WEB WHOIS
+				PRE_ARGUMENT=""
+				! test -z "$HOST" && PRE_ARGUMENT="$PRE_ARGUMENT -h $HOST"
+				! test -z "$PORT" && PRE_ARGUMENT="$PRE_ARGUMENT -p $PORT"
+				RESULT=$(whois "$PRE_ARGUMENT $DOMAIN")
+				echo "$RESULT" | grep -i "no whois server" > /dev/null
+				if [ $? -eq 0 ]; then
+					RESULT=$("${WHOIS_WORKING_DIR}/api/getwhois.sh" "$DOMAIN")
+				fi
 			fi
 		fi
 		echo "$RESULT"
