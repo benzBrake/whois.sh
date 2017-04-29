abf234d36b35d20f26ea050deccc98a41778c2ee
benzBrake <github-benzBrake@woai.ru>
2017-04-29 13:24:06 +0800

    add al whois

---
+++ api/al.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+DOMAIN=$1
+RESULT=$(curl -s "https://albaniandomains.al/whmcs/whois.php?domain=$DOMAIN")
+if [ -z "$(echo "$RESULT" | grep registered)" ]; then
+	echo "Congratulations, $DOMAIN is available!"
+else
+	echo "Sorry, host.al. is already registered. "
+	echo "$RESULT" | grep Nameserver | sed 's#\s*<br />\s*##g'
+fi
