5f205142e64e25cb61e72fbd72dd768219d67aef
benzBrake <github-benzBrake@woai.ru>
2018-03-09 11:51:23 +0800

    fix not such file error

--- inc/ip.sh
+++ inc/ip.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 DOMAIN="$@"
-RESULT=$($WHOIS_WORKING_DIR/api/tcp.sh -host whois.iana.org -port 43 -data $DOMAIN)
+RESULT=$($WHOIS_WORKING_DIR/inc/tcp.sh -host whois.iana.org -port 43 -data $DOMAIN)
 PREFIX=$(echo "$DOMAIN" | sed 's#\..*##')
-! test -e "${WHOIS_WORKING_DIR}/api/prefix.list" && touch "${WHOIS_WORKING_DIR}/api/prefix.list"
-HANDLER=$(cat $WHOIS_WORKING_DIR/api/prefix.list | grep "PREFIX${PREFIX}" | sed "s#^.*=##")
+! test -e "${WHOIS_WORKING_DIR}/prefix.list" && touch "${WHOIS_WORKING_DIR}/prefix.list"
+HANDLER=$(cat $WHOIS_WORKING_DIR/prefix.list | grep "PREFIX${PREFIX}" | sed "s#^.*=##")
 test -z "$HANDLER" && {
 	HANDLER=$(echo -e "$RESULT" | grep 'whois:' | sed 's#^[^ ]* *##')
 	test -z "$HANDLER" && {
@@ -12,5 +12,5 @@ test -z "$HANDLER" && {
 	}
 	echo "PREFIX${PREFIX}=${HANDLER}" >> "$WHOIS_WORKING_DIR/prefix.list"
 }
-RESULT=$($WHOIS_WORKING_DIR/api/tcp.sh -host $HANDLER -port 43 -data $DOMAIN)
+RESULT=$($WHOIS_WORKING_DIR/inc/tcp.sh -host $HANDLER -port 43 -data $DOMAIN)
 echo -e "$RESULT"
