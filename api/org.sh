09679eff7437fbb8cafe0a4f1a77a25f78169b17
benzBrake <github-benzBrake@woai.ru>
2018-03-14 16:22:32 +0800

    fix .org whois

--- api/org.sh
+++ api/org.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 DOMAIN=$1
-SERVER=$($WHOIS_WORKING_DIR/inc/tcp.sh -host whois.verisign-grs.com -port 43 -data "$DOMAIN")
-SERVER=$(echo -e "$SERVER" | grep "Registrar WHOIS Server" | awk '{print $4}' | sed "s#\r##g")
+SERVER="whois.pir.org"
 $WHOIS_WORKING_DIR/inc/tcp.sh -host "$SERVER" -port 43 -data "$DOMAIN"
