436ad0f0914d62d5bd7e76ae2644caff83dceb31
benzBrake <github-benzBrake@woai.ru>
2018-03-24 21:53:37 +0800

    fix unregisted .com domains whois output

--- api/com.sh
+++ api/com.sh
@@ -12,11 +12,13 @@ if [[ ! -z "$SDM" ]]; then
 	if [[ "$SLD" == "br" || "$SLD" == "cn"  || "$SLD" == "co" || "$SLD" == "de" || "$SLD" == "eu" || "$SLD" == "gr" || "$SLD" == "jpn" || "$SLD" == "mex" || "$SLD" == "ru" || "$SLD" == "sa" || "$SLD" == "uk" || "$SLD" == "us" || "$SLD" == "za" ]]; then
 		SERVER=whois.centralnic.com
 	else
-		SERVER=$($WHOIS_WORKING_DIR/inc/tcp.sh -host whois.verisign-grs.com -port 43 -data "$DOMAIN")
-		SERVER=$(echo -e "$SERVER" | grep "Registrar WHOIS Server" | awk '{print $4}' | sed "s#\r##g")
+		echo "Do not support this domain."
 	fi
 else
-	SERVER=$($WHOIS_WORKING_DIR/inc/tcp.sh -host whois.verisign-grs.com -port 43 -data "$DOMAIN")
-	SERVER=$(echo -e "$SERVER" | grep "Registrar WHOIS Server" | awk '{print $4}' | sed "s#\r##g")
+	RESULTS=$($WHOIS_WORKING_DIR/inc/tcp.sh -host whois.verisign-grs.com -port 43 -data "$DOMAIN")
+	SERVER=$(echo -e "$RESULTS" | grep "Registrar WHOIS Server" | awk '{print $4}' | sed "s#\r##g")
+	if [[ -z "$SERVER" ]]; then
+		echo -e "$RESULTS"
+	fi
 fi
 $WHOIS_WORKING_DIR/inc/tcp.sh -host "$SERVER" -port 43 -data "$DOMAIN"
